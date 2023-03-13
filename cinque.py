b022595e7759c513dde0978275ca2e752880e9de
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2023-03-13 12:58:11 +0000

    commit

---
+++ cinque.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+import datetime
+
+@app.route('/') # L'utente richiede l'home page
+def index():
+  return render_template("index4.html", ora = datetime.datetime.now())
+
+@app.route('/pippo')
+def pippo():
+    return('Pippo non esiste')
+
+@app.route('/mappa')
+def mappa():
+    return render_template("index5.html")
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
---
+++ tre.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/') # L'utente richiede l'home page
+def index():
+  return render_template("index3.html")
+
+@app.route('/pippo')
+def pippo():
+    return('Pippo non esiste')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
